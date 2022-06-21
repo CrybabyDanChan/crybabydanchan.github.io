10a1ef39391cd7de16cd1a0f80c4a326f050fd0f
Daniil Stepanko <crysiscaramel@gmail.com>
2022-06-21 20:10:02 +0300

    Создал программу для входа в свой аккаунт

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-const order = {
-    morning: false,
-    dinner: false,
-    evning: false,
-    night: false,
+// const car = undefined; // если есть символы - то true, если пустая строка - false
+// const day = 0; //если есть число - то true, если будет 0 - false
+// const something = undefined; // false
+// const notning = null; //false
+
+// const result1 = day || car; // ИЛИ возвращает первое true
+// const result2 = day && car; //И если все значения true - веренет последнее, если какое то значение false - то вернет предыдущее или его(если оно первое)
+// const result3 = !day; // отрицание
+// const result4 = day ?? car; // возвращает первое значение которое не равно null или undefined
+
+const acount = {
+    login: null,
+    password: null,
+    phone: null,
+    admin: fale,
 }
 
-const setDayInterval = function(data, key) {
-    data[key] = true;
-}
-
-const initialDayInterval = function(data, key) {
-    data[key] = false;
-}
-
-const getActionDay = function(data) {
-    if(data.morning) {
-        console.log('Выпью кофе')
-    } else if (data.dinner) {
-        console.log('сЪем омлет')
-    } else if (data.evning) {
-        console.log('пойду гулять')
-    } else if (data.night) {
-        console.log('буду спать')
+function getAction() {
+    if (!acount.login && !acount.admin) {
+        console.log('Пожалуйста введите логин!')
+    } else if (!acount.password) {
+        console.log('Пожалуйста введите пароль!')
+    } else if (!acount.phone) {
+        console.log('Пожалуйста введите телефон!')
     } else {
-        console.log('день не начался')
+        console.log('Добро пожаловать!')
     }
 }
 
-setDayInterval(order, 'dinner')
-getActionDay(order)
-initialDayInterval(order, 'dinner')
-setDayInterval(order, 'night')
-getActionDay(order)
+acount['login'] = 'developer';
+acount['password'] = '1234';
+acount['phone'] = '7999999999';
+
+getAction()
